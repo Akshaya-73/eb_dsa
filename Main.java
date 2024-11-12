5f9794777bd48906198041f2f05583c311c982dd
Akshaya-73 <238r1a05bz@cmrec.ac.in>
2024-11-12 10:29:59 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+import util.java.Scanner;
+class Main {
+  Public static void main(String[] args);
+  {
+  Scanner sc=new Scanner(system.in);
+  int x=sc.nextInt();
+  int y= sc.nextInt();
+  if (x <= y) {
+  System.out.println("YES");
+}
+else {
+  System.out.println("NO");
+}
+}
+}
